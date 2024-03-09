d9efcbc11bd68f04117c6d5fe95b8380f7f3817d
Trainingwolf8878 <dailin20000730@gmail.com>
2024-03-09 23:49:53 +0000

    e1

---
+++ Working space set up.R
@@ -0,0 +1,14 @@
+## Set up work space
+
+install.packages("usethis")
+install.packages("gitcreds")
+
+## Load the package
+
+library(usethis)
+library(gitcreds)
+
+## Working zone
+
+create_github_token()
+gitcreds_set()
